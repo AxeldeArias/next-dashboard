938f96d230b05fe12e0b9ec3c3b1e009d67edab4
Axel <axel@ip-192-168-100-41.us-west-1.compute.internal>
2023-11-06 11:01:41 -0300

    feat: initial commit

---
+++ app/dashboard/cards.tsx
@@ -0,0 +1,25 @@
+"use server";
+import { Card } from "@/app/ui/dashboard/cards";
+import { fetchCardData } from "@/app/lib/data";
+
+export default async function Cards() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
+  return (
+    <>
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card
+        title="Total Customers"
+        value={numberOfCustomers}
+        type="customers"
+      />
+    </>
+  );
+}
